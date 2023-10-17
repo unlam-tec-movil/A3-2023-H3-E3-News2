131ef3106375ce04872c4ee2f96c3ff7b3302884
Nahuel Palacio <palacionahuel202@gmail.com>
2023-10-17 19:17:20 -0300

    1
    1

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/mobile2/data/weather/network/WeatherResponse.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/mobile2/data/weather/network/WeatherResponse.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:app/src/main/java/ar/edu/unlam/mobile/scaffold/mobile2/data/weather/network/WeatherResponse.kt
 package ar.edu.unlam.mobile.scaffold.mobile2.data.weather.network
-========
-package ar.edu.unlam.mobile2.weatherapi.data
->>>>>>>> develop:app/src/main/java/ar/edu/unlam/mobile/scaffold/mobile2/weatherapi/data/WeatherResponse.kt
 
 import com.google.gson.annotations.SerializedName
 
